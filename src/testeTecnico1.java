e11b701b80298010d9a2c4407dc4e17ab7e73b0f
AlexVicente1 <alexxvicente40@gmail.com>
2025-07-09 11:17:10 -0300

    Nova atualização

---
+++ src/testeTecnico1.java
@@ -0,0 +1,13 @@
+public class testeTecnico1 {
+
+    public static void main(String[] args) {
+        // Faça um FOR que imprima de 1 a 100 apenas múltiplos de 5
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 5 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
---
+++ src/testeTecnico2.java
@@ -0,0 +1,12 @@
+public class testeTecnico2 {
+
+    public static void main(String[] args) {
+        System.out.println(isEven(4)); // teste
+        System.out.println(isEven(5)); // teste
+    }
+
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
+
